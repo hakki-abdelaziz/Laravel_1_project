7b865bb171a0d7aa7dded03334650546ea825f26
hakki-abdelaziz <abdalazizhakki@gmail.com>
2024-06-14 13:24:59 +0100

    edit profile page

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,21 +1,30 @@
 
-<x-master title='Profile '>
+<x-master title='User Profile '>
 
-    <h1>{{ Str::upper($profile->name) }} Profile </h1>
-    <img class="card-img-top" src="{{ asset('storage/'.$profile->image) }}" alt="it's ME 😁" />
-    Mr/Mme. <b>{{ Str::upper($profile->name) }} </b></br>
-    id is : {{$profile->id}} </br>
-    email is : {{$profile->email}} </br>
-    bio : {{$profile->bio}} </br>
-    <form action="{{ route('profiles.destroy',$profile->id) }}" method="post">
-        @method('DELETE')
-        @csrf
-        <button class="btn btn-danger btn-sm float-end">DELETE !</button>
-    </form>
-    <form action="{{ route('profiles.edit',$profile->id) }}" method="GET">
-        @csrf
-        <button class="btn btn-primary btn-sm float-end mx-2">UPDATE !</button>
-    </form>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="card my-d py-4">
+                <img class="card-img-top w-25 mx-auto" src="{{ asset('storage/'.$profile->image) }}" alt="it's ME 😁" />
+                <div class="card-body text-center">
+                    <h4 class="card-title">#{{$profile->id}} <b>{{ Str::upper($profile->name) }}</b></h4>
+                    <p class="card-text">Email : {{$profile->email}}</p>
+                    <p class="card-text">Bio : {{$profile->bio}}</p>
+                    <p class="card-text">Created at {{$profile->created_at->format('d-m-y')}}</p>
+                </div>
+            </div>
+            <div class="card-foot border-top px-3 py-3 bg-light">
+                <form action="{{ route('profiles.destroy',$profile->id) }}" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <button class="btn btn-danger btn-sm float-end">DELETE !</button>
+                </form>
+                <form action="{{ route('profiles.edit',$profile->id) }}" method="GET">
+                    @csrf
+                    <button class="btn btn-primary btn-sm float-end mx-2">UPDATE !</button>
+                </form>
+            </div>
+        </div>
+    </div>
 
 </x-master>
 
--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,22 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Information;
 use App\Http\Controllers\LoginController;
 
-Route::get('/', [homeController::class, 'index'])->name('homepage');
 
 Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles.index');
 
 Route::get('/profiles/create', [ProfileController::class, 'create'])->name('create');
 
 Route::get('/profiles/{profile}', [ProfileController::class, 'show'])->where('profile','\d+')->name('profiles.show');
+
+Route::delete('/profiles/{profile}', [ProfileController::class, 'destroy'])->name('profiles.destroy');
+
+Route::get('/profiles/{profile}/edit', [ProfileController::class, 'edit'])->name('profiles.edit');
+
+Route::put('/profiles/{profile}', [ProfileController::class, 'update'])->name('profiles.update');
+
+
+
+
 
 Route::post('/profiles/store', [ProfileController::class, 'store'])->name('store');
 
@@ -22,11 +31,11 @@ Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
 
-Route::delete('/profiles/{profile}', [ProfileController::class, 'destroy'])->name('profiles.destroy');
 
-Route::get('/profiles/{profile}/edit', [ProfileController::class, 'edit'])->name('profiles.edit');
 
-Route::put('/profiles/{profile}', [ProfileController::class, 'update'])->name('profiles.update');
+
+
+Route::get('/', [homeController::class, 'index'])->name('homepage');
 
 Route::get('/settings', [Information::class, 'index'])->name('settings.index');
 
