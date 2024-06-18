b06324ea6264aaf5e7e7cfc329d1a583b6412bf3
hakki-abdelaziz <abdalazizhakki@gmail.com>
2024-06-18 01:49:15 +0100

    Publication P:1

---
+++ database/factories/PublicationFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Publication>
+ */
+class PublicationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+            'image' => null, // You can modify this to generate random image paths if needed
+        ];
+    }
+}
+
+
+
+
